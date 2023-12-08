fca14a88b95c73a0157ca2288e3c6cbebb8fbf3a
srg190 <sudhiranjang@gmail.com>
2023-12-09 01:38:33 +0530

    --useDebounce-cusHook-implement-searchbar

--- src/Hooks/useDebounce.tsx
+++ src/Hooks/useDebounce.tsx
@@ -1,7 +1,16 @@
-interface Debounce {
-  value: any;
-  milliseconds: number;
-}
-export const useDebounce = ({ value, milliseconds }: Debounce) => {
-    
+import { useState, useEffect } from "react";
+
+export const useDebounce = (value: string, milliseconds: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, milliseconds);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, milliseconds]);
+
+  return debounceValue;
 };
