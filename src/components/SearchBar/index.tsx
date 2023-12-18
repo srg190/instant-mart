1728eddd03abb55e1780de3568108dc48f2058b2
srg190 <sudhiranjang@shriffle.com>
2023-12-18 12:58:32 +0530

    implemeted useDebounce custom Hook searchbar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { Input } from "./index.style";
+import { useDebounce } from "Hooks/useDebounce";
 let cnt = 0;
 const SearchBar = () => {
   const [keywords, setKeywords] = useState<string>("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeywords(e.target.value);
   };
+  const debounceVal = useDebounce(keywords, 300);
   // will throttle here
+  console.log(debounceVal, cnt++);
   return (
     <>
       <Input
         type="text"
         onChange={(e) => handleChange(e)}
         placeholder="search..."
+        value={keywords}
       />
     </>
   );
